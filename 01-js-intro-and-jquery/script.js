54b52afb51f88281bcf09e025193321671863050
rodixxi <rodixxi@gmail.com>
2015-02-26 16:13:08 -0300

    bottom event

--- 01-js-intro-and-jquery/script.js
+++ 01-js-intro-and-jquery/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
-	$('.title').show(3000);
-	
-});
-$(".title").blur(function(){
-    	$('.alias').focus(function() {
-  			$('.alias').css('cursor', 'wait');
-  		});
+	$('.title').show(3000, function(){
+		$('.alias').focus();
 	});
+	$('#txtButtom').click(function(){
+	var name = $('#txt').val();
+	var url = "http://bootcamp.aws.af.cm/welcome/" +name.toString();
+	$.ajax(url, {
+		type: 'GET',
+        dataType: 'json',
+          success: function(){
+          	var resp = $('responce');
+          	$('#txt').val('resp');
+          },
+        });
+      });
+});
