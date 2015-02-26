c460d9e4daed8cd45a71ea6a68c928db2443bb08
rodixxi <rodixxi@gmail.com>
2015-02-25 22:45:46 -0300

    texbox added

--- 01-js-intro-and-jquery/script.js
+++ 01-js-intro-and-jquery/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
 	$('.title').show(3000);
+	
 });
+$(".title").blur(function(){
+    	$('.alias').focus(function() {
+  			$('.alias').css('cursor', 'wait');
+  		});
+	});
